29938dc88a51b1f570467a591fe2cde16a3e6648
princehaku <baizhongwei@gmail.com>
2013-02-28 14:12:27 +0800

    pypi 加入

--- setup.py
+++ setup.py
@@ -2,22 +2,33 @@ NAME = 'PyRailgun'
 VERSION = '0.10'
 DESCRIPTION = "Fast Craweler for Python"
 LONG_DESCRIPTION = """\
+this is a simple Craweler Framework
+
+from railgun import RailGun
+
+railgun = RailGun()
+railgun.setTask(file("testsite.yaml"));
+railgun.fire();
+nodes = railgun.getShells('default')
+print nodes
+
+see test for more infomation
 """
 AUTHOR = "zhongwei bai"
 AUTHOR_EMAIL = 'baizhongwei@163.com'
 LICENSE = "MIT"
 PLATFORMS = "Any"
-URL = "http://3haku.net"
+URL = "https://github.com/princehaku/pyrailgun"
 DOWNLOAD_URL = "http://lab.3haku.net/pyrailgun/%s-%s.tar.gz" % (NAME, VERSION)
 CLASSIFIERS = [
-    "Development Status :: Production/Stable",
+    "Development Status :: 4 - Beta",
     "Intended Audience :: Developers",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2.7",
+    "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
     "Topic :: Software Development :: Libraries :: Python Modules",
-    "Topic :: Crawel Processing :: Yaml",
 ]
 REQUIRS = [
     'pyyaml',

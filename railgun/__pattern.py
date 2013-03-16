8f42747256c5db493544277b27e6a3654328db34
Hontyu Bhe <mfthits@gmail.com>
2013-03-16 17:30:14 +0800

    修复语法错误, 添加doctest

--- railgun/__pattern.py
+++ railgun/__pattern.py
@@ -12,35 +12,51 @@ class Pattern:
         assert (task_entry != None), "task_entry can't be None"
         self.task_entry = task_entry
         self.shell = shell
-        self.logger = Logger.GetLogger()
+        self.logger = Logger.getLogger()
 
     # deep.. and deep recursion
     def convertPattern(self, area, resource_str = None):
-        # task_entry must have specify field
-        if None == self.task_entry.get(area):
+        """
+        :param area: dict
+        :param resource_str: string
+        :return:
+        >>> Pattern({'url':'http://is.me/${0,1}'}).convertPattern('url')  # doctest: +ELLIPSIS
+        load logging configure from logging.conf
+        [...] (INFO) : Pattern Found as 0,1
+        ['http://is.me/0', 'http://is.me/1']
+        """
+
+        # specify field required in task_entry
+        if not self.task_entry.get(area, None):
             return None
-        text = self.task_entry.get(area)
-        # set resource_str as new text resource
-        if resource_str != None:
-            text = resource_str
-        matched = re.findall(r'\$\{(.*?)\}', text)
+
+        text = resource_str\
+            if resource_str is not None\
+            else self.task_entry.get(area)
+
+        pattern = re.compile(r'\$\{(.*?)\}')
+
+        matched = pattern.findall(text)
         # if doesn't match. just return it
-        if len(matched) == 0:
+        if not matched:
             return text
+
         self.logger.info("Pattern Found as " + matched[0])
-        convetedstrs = []
+
+        converted_strings = []
         # support ${#} as shell's Field
-        if matched[0][0:1] == '#':
-            keyname = matched[0][1:]
+        if matched[0].startswith('#'):
+            key_name = matched[0][1:]
             assert self.shell != None, "shell can't be empty when using #"
-            if self.shell.get(keyname) == None:
+            if self.shell.get(key_name) == None:
                 return None
-            assert len(self.shell[keyname]) <= 1, " shell 'src length is greater than 1"
-            replacedst = ""
-            if len(self.shell[keyname]) == 1:
-                replacedst = self.shell[keyname][0]
-            new_str = re.sub(r'\$\{(.*?)\}', replacedst, text, 1)
-            convetedstrs.append(new_str)
+            assert len(self.shell[key_name]) <= 1, " shell 'src length is greater than 1"
+
+            replacedst = self.shell[key_name][0]\
+                if len(self.shell[key_name]) == 1\
+                else ""
+
+            converted_strings.append(pattern.sub(replacedst, text, 1))
 
         # expand ${n,m} as n,n+1,n+2...m
         if re.match(r'\d*,\d*', matched[0]):
@@ -49,16 +65,15 @@ class Pattern:
             lower = int(regxp.group(1))
             max = int(regxp.group(2))
             for i in range(lower, max + 1):
-                new_str = re.sub(r'\$\{(.*?)\}', str(i), text, 1)
-                convetedstrs.append(new_str)
+                new_str = pattern.sub(str(i), text, 1)
+                converted_strings.append(new_str)
 
         # recursion
-        convetedstrs_new = []
-        for s_str in convetedstrs:
+        converted_list = []
+        for s_str in converted_strings:
             converted_sub = self.convertPattern(area, resource_str=s_str)
-            if isinstance(converted_sub, list) :
-                for new_str in converted_sub:
-                    convetedstrs_new.append(new_str)
-            else :
-                convetedstrs_new.append(s_str)
-        return convetedstrs_new
+            if not isinstance(converted_sub, list):
+                converted_sub = [converted_sub]
+            converted_list.extend(converted_sub)
+
+        return converted_list
